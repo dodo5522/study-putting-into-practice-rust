6937448783b661c7cd1000d0079c1fc29fae1a31
Takashi Ando <dodo5522@gmail.com>
2022-10-16 21:01:43 +0900

    [second] Set Ascending/Descending to sort function

--- bitonic_sorter/src/second.rs
+++ bitonic_sorter/src/second.rs
@@ -1,9 +1,11 @@
-pub fn sort<T: Ord>(x: &mut[T], up: bool) {
+use crate::SortOrder;
+
+pub fn sort<T: Ord>(x: &mut[T], order: SortOrder) {
     if x.len() > 1 {
         let middle_index = x.len() / 2;
-        sort(&mut x[..middle_index], true);
-        sort(&mut x[middle_index..], false);
-        sort_sub(x, up);
+        sort(&mut x[..middle_index], SortOrder::Ascending);
+        sort(&mut x[middle_index..], SortOrder::Descending);
+        sort_sub(x, order == SortOrder::Ascending);
     }
 }
 
@@ -29,11 +31,12 @@ fn swap_if_needed<T: Ord>(x: &mut[T], up: bool) {
 mod tests {
     use std::vec;
     use super::sort;
+    use crate::SortOrder;
 
     #[test]
     fn sort_u32_ascending() {
         let mut array: Vec<u32> = vec![1, 3, 65, 21, 2, 9, 7, 0];
-        sort(&mut array, true);
+        sort(&mut array, SortOrder::Ascending);
 
         assert_eq!(array, vec![0, 1, 2, 3, 7, 9, 21, 65]);
     }
@@ -41,7 +44,7 @@ mod tests {
     #[test]
     fn test_u32_descending() {
         let mut array: Vec<u32> = vec![1, 3, 65, 21, 2, 9, 7, 0];
-        sort(&mut array, false);
+        sort(&mut array, SortOrder::Descending);
 
         assert_eq!(array, vec![65, 21, 9, 7, 3, 2, 1, 0]);
     }
@@ -49,7 +52,7 @@ mod tests {
     #[test]
     fn sort_str_ascending() {
         let mut array: Vec<&str> = vec!["hoge", "fuga", "fugo", "hage"];
-        sort(&mut array, true);
+        sort(&mut array, SortOrder::Ascending);
 
         assert_eq!(array, vec!["fuga", "fugo", "hage", "hoge"]);
     }
@@ -57,7 +60,7 @@ mod tests {
     #[test]
     fn test_str_descending() {
         let mut array: Vec<&str> = vec!["hoge", "fuga", "fugo", "hage"];
-        sort(&mut array, false);
+        sort(&mut array, SortOrder::Descending);
 
         assert_eq!(array, vec!["hoge", "hage", "fugo", "fuga"]);
     }
